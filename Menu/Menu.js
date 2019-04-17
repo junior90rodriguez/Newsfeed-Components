ca65d2c7e1680c56f01c88b955190ef7690c3c7b
Junior <junior.arodriguez@icloud.com>
2019-04-17 13:16:56 -0500

    refrenced .menu class and .menu-button class

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,7 +5,11 @@
 
 // // Start Here: Create a reference to the ".menu" class
 
-// const menu;
-// // create a reference to the ".menu-button" class
-// const menuButton;
-// // // Using your menuButton reference, add a click handler that calls toggleMenu
+const menu = document.querySelector('.menu');
+
+// create a reference to the ".menu-button" class
+
+const menuButton = document.querySelector('.menu-button');
+
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', )
